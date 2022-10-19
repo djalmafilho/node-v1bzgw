52b11c73b72b1035f6f36e3be1e6ea9c6cf5269e
djalmafilho <113950165+djalmafilho@users.noreply.github.com>
2022-10-19 20:01:07 -0300

    2 files modified

--- nome-do-projeto/src/app/clientes/cliente-lista/cliente-lista.component.ts
+++ nome-do-projeto/src/app/clientes/cliente-lista/cliente-lista.component.ts
@@ -3,13 +3,23 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-cliente-lista',
   templateUrl: './cliente-lista.component.html',
-  styleUrls: ['./cliente-lista.component.css']
+  styleUrls: ['./cliente-lista.component.css'],
 })
 export class ClienteListaComponent implements OnInit {
+  clientes = [
+    {
+      nome: 'José',
+      fone: '11223344',
+      email: 'jose@email.com',
+    },
+    {
+      nome: 'Maria',
+      fone: '22334455',
+      email: 'maria@email.com',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
